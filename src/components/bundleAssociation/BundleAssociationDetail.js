b27599f74dc708afdb0adc848992cedc1513977e
warrior14 <ironaddicts14@gmail.com>
2021-07-29 12:44:02 -0500

    added a ExerciseTimerModal.js and made a modal pop up when the user clicks on the start workout button

--- src/components/bundleAssociation/BundleAssociationDetail.js
+++ src/components/bundleAssociation/BundleAssociationDetail.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react"
 import { BundleAssociationContext } from "./BundleAssociationProvider.js"
 import "./BundleAssociation.css"
 import { useParams } from "react-router-dom"
+import { ExerciseTimerModal } from "./ExerciseTimerModal"
 
 export const BundleAssociationDetail = () => {
   const { getBundleAssociationExerciseById, currentBundle, getBundleExercises, bundleExercises } = useContext(BundleAssociationContext)
@@ -9,6 +10,8 @@ export const BundleAssociationDetail = () => {
 	const [bundleAssociationExercise, setBundleAssociationExercise] = useState({})
 
 	const {bundleAssociationId} = useParams();
+
+  const [timerIsDisplayed, setTimerIsDisplayed ] = useState(false)
 
   useEffect(() => {
     // getBundleAssociationExerciseById(bundleAssociationId)
@@ -18,25 +21,34 @@ export const BundleAssociationDetail = () => {
     getBundleExercises(currentBundle.id);
   }, [])
 
+  let exerciseTimerModal;
+  if (timerIsDisplayed) {
+    exerciseTimerModal = <ExerciseTimerModal />
+  } else {
+    exerciseTimerModal = null;
+  }
+
   return (
     <>
 
-        <button onClick={() => {console.log('bundle exercises', bundleExercises)}}>console log</button>
+        {/* <button onClick={() => {console.log('bundle exercises', bundleExercises)}}>console log</button> */}
         
         <h1 className="">{currentBundle.name}</h1>
         {
 
           bundleExercises.map((exercise) => {
             return <>
+                    {
+                     exerciseTimerModal
+                    }
                     <section className="bundle"> 
                       <h3 className="">{exercise.name}</h3>
                       <p className="">{exercise.description}</p>
                     </section>
-
-
-                    <h3>Timer Countdown:</h3>
-                    <button>Start Workout</button>
-                    <button>Stop Workout</button>
+                    <button onClick={() => {
+                      setTimerIsDisplayed(true)
+                      console.log("timer state", timerIsDisplayed )
+                    }}>Start Workout</button>
                     <h3>Sets</h3>
                     <h3>Set Time</h3>
                     <h3>Cool Down Time</h3>
